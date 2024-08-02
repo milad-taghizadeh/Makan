b0d115d8365bee4b727c6923ff6012f36b59af98
Amirreza <Amirreza.80.sadeghi@gmail.com>
2024-08-02 16:27:32 +0330

    fix: Retrieve newest OTP by expiration time Modified findByPhone method to return the most recent OTP based on the expiresIn field, ensuring that the latest OTP is retrieved for a given phone number.

--- src/modules/auth/otp.repository.ts
+++ src/modules/auth/otp.repository.ts
@@ -45,6 +45,9 @@ export class OtpRepository implements Repository<Otp> {
     return await this.prismaService.otp.findFirst({
       where: {
         phoneNumber
+      },
+      orderBy: {
+        expiresIn: 'desc'
       }
     })
   }
