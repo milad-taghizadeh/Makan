c9c70f67316512764d535d4529fab1e48b051d43
hoja1381 <h.jahandideh2@gmail.com>
2024-08-06 01:35:32 +0330

    feat :token service

--- src/modules/auth/token.service.ts
+++ src/modules/auth/token.service.ts
@@ -4,13 +4,24 @@ import { CookiePayload } from './types/payload';
 
 @Injectable()
 export class TokenService {
-  constructor(private jwtService: JwtService) {}
-  async createOtpToken(payload: CookiePayload) {
+  constructor(
+    private jwtService: JwtService
+  ) { }
+
+  createOtpToken(payload: CookiePayload): string {
     const token = this.jwtService.sign(payload, {
       secret: process.env.OTP_TOKEN_SECRET,
       expiresIn: 60 * 2,
     });
     return token;
   }
+  verifyToken(token: string): CookiePayload {
+    try {
+      const payload: CookiePayload = this.jwtService.verify(token)
+      return payload
+    } catch {
+      return
+    }
 
+  }
 }
