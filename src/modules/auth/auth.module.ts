84a63df6e67d23b6231b40ebff75f4139e2c1462
hoja1381 <h.jahandideh2@gmail.com>
2024-08-06 00:56:24 +0330

    feat :add jwt module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,21 +1,37 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from 'src/database/database.service';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { TokenService } from './token.service';
 import { OtpRepository } from './otp.repository';
 import { UserRepository } from '../user/user.repository';
+import { VerifyToken } from 'src/common/middlewares/verify-token.middleware';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  controllers: [AuthController],
+  imports: [
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get("OTP_TOKEN_SECRET"),
+      }),
+      inject: [ConfigService],
+      global: true,
+    }),
+  ],
+  controllers: [
+    AuthController,
+  ],
   providers: [
     AuthService,
     PrismaService,
-    JwtService,
     TokenService,
     OtpRepository,
     UserRepository
   ],
 })
-export class AuthModule { }
+export class AuthModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(VerifyToken).forRoutes('*');
+  }
+}
