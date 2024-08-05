a8cef0f54615a86893ae2215da1a5e1c8d5d636a
hoja1381 <h.jahandideh2@gmail.com>
2024-08-06 00:55:44 +0330

    feat  : add guard to agent controller

--- src/modules/agent/agent.controller.ts
+++ src/modules/agent/agent.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { AgentService } from './agent.service';
 import { CreateAgentDto } from './dto/create-agent.dto';
 import { UpdateAgentDto } from './dto/update-agent.dto';
+import { JwtGuard } from 'src/common/guards/auth.guard';
+import { User } from 'src/common/decorators/user.decorator';
+import { CookiePayload } from '../auth/types/payload';
 
 @Controller('agent')
+@UseGuards(JwtGuard)
 export class AgentController {
-  constructor(private readonly agentService: AgentService) {}
+  constructor(private readonly agentService: AgentService) { }
 
   @Post()
-  create(@Body() createAgentDto: CreateAgentDto) {
+  create(
+    @Body() createAgentDto: CreateAgentDto,
+    @User() user: CookiePayload
+  ) {
+    console.log(user);
     return this.agentService.create(createAgentDto);
   }
 
