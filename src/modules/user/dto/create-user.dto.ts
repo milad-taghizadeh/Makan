e5207137e3ad6b89d01d371995ea191a557fee7a
Amirreza <Amirreza.80.sadeghi@gmail.com>
2024-09-01 23:35:56 +0330

    feat: add user service

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { IsOptional, IsString } from "class-validator";
+
+export class CreateUserDto {
+    @IsString()
+    @IsOptional()
+    firstname: string;
+ 
+    @IsString()
+    @IsOptional()
+    lastName: string;
+ 
+    @IsString()
+    @IsOptional()
+    email: string;
+ 
+    @IsString()
+    phone: string;
+  }
